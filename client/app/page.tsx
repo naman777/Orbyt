dfa86c6c3fac23842390f7c365d240d8231bf892
Naman Kundra <namankundra6@gmail.com>
2024-10-27 19:06:06 +0530

    responive home page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,21 +1,22 @@
 import { Appbar } from "@/components/Appbar";
 import { Hero } from "@/components/Hero";
-import { HeroVideo } from "@/components/HeroVideo";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="min-h-screen pb-48 overflow-hidden">  {/* Changed className here */}
+    <main className="min-h-screen pb-48 overflow-hidden">
       <Appbar />
-      <div className="pt-8 flex justify-between">
+      <div className="pt-8 flex flex-col md:flex-row justify-between md:w-full ml-4">
         <Hero />
-        <Image
-          src="https://res.cloudinary.com/zapier-media/image/upload/q_auto/f_auto/v1726210651/Homepage%20%E2%80%94%20Sept%202024/homepage-hero_vvpkmi.png"
-          width={500}
-          height={500}
-          alt="main"
-          className="mr-12"
-        />
+        <div className="mt-8 md:mt-0 md:ml-12 flex justify-center ">
+          <Image
+            src="https://res.cloudinary.com/zapier-media/image/upload/q_auto/f_auto/v1726210651/Homepage%20%E2%80%94%20Sept%202024/homepage-hero_vvpkmi.png"
+            width={500}
+            height={500}
+            alt="main"
+            className=""
+          />
+        </div>
       </div>
     </main>
   );
